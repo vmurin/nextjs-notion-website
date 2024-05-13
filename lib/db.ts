a884a5cbb7004593bd62091e649250f1f60c3a58
Vladimir Murin <vladimir@murin-online.de>
2024-05-14 00:34:35 +0200

    Check redisUrl not empty

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,8 @@ import KeyvRedis from '@keyvhq/redis'
 import { isRedisEnabled, redisNamespace, redisUrl } from './config'
 
 let db: Keyv
-if (isRedisEnabled) {
+if (isRedisEnabled && redisUrl) {
+  console.log(`Connecting to Redis at ${redisUrl}`)
   const keyvRedis = new KeyvRedis(redisUrl)
   db = new Keyv({ store: keyvRedis, namespace: redisNamespace || undefined })
 } else {
